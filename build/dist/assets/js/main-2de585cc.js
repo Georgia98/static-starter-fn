2a95a73957fcd98161e5c8ae04880a1ea95f557d
Georgia98 <32138103+Georgia98@users.noreply.github.com>
2017-09-25 08:36:11 +0100

    Add files via upload

---
+++ build/dist/assets/js/main-2de585cc.js
@@ -0,0 +1,14 @@
+/** StaticStarter - Bespoke Starting Point - By: https://wearedamage.com */$(function(){
+	var win = $(window),
+		doc = $(document),
+		body = $('body'),
+		$self = $(this);
+		
+	// File upload
+    $('.file-upload').change(function() {
+        var filepath = this.value;
+        var m = filepath.match(/([^\/\\]+)$/);
+        var filename = m[1];
+        $(this).parent('label').siblings('span').html(filename);
+    });
+});
